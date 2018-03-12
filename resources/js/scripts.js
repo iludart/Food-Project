91412831819dd25df63f0f95796b78ea4d308963
iludart <peter.penev@hotmail.com>
2018-03-12 12:59:19 +0200

    Added jquery scroll to buttons

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -9,36 +9,48 @@ $(document).ready(() => {
             }
         },
         offset: 70
-    })
+    });
 
     // NAVIGATION SCROLL
-    $('.home-link').click(() => {
+    $('#home-link').click(() => {
         var scrollto = 0;
         $('html, body').animate({scrollTop:scrollto}, 1000);
-    })
+    });
 
-    $('.features-link').click(() => {
+    $('#features-link').click(() => {
         var offset = $('.js--section-features').offset();
         var scrollto = offset.top - 60;
         $('html, body').animate({scrollTop:scrollto}, 1000);
-    })
+    });
 
-    $('.steps-link').click(() => {
+    $('#steps-link').click(() => {
         var offset = $('.js--section-works').offset();
         var scrollto = offset.top - 60;
         $('html, body').animate({scrollTop:scrollto}, 1000);
-    })
+    });
 
-    $('.cities-link').click(() => {
+    $('#cities-link').click(() => {
         var offset = $('.js--section-cities').offset();
         var scrollto = offset.top - 60;
         $('html, body').animate({scrollTop:scrollto}, 1000);
-    })
+    });
 
-    $('.contact-link').click(() => {
+    $('#contact-link').click(() => {
         var offset = $('.js--section-contact').offset();
         var scrollto = offset.top - 60;
         $('html, body').animate({scrollTop:scrollto}, 1000);
-    })
+    });
+
+    $('#features-button-link').click(() => {
+        var offset = $('.js--section-features').offset();
+        var scrollto = offset.top - 60;
+        $('html, body').animate({scrollTop:scrollto}, 1000);
+    });
+
+    $('#plans-link').click(() => {
+        var offset = $('.js--section-plans').offset();
+        var scrollto = offset.top - 60;
+        $('html, body').animate({scrollTop:scrollto}, 1000);
+    });
 });
 
